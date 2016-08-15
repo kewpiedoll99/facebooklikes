6e869390add5a62e5314b5710040d5ec4f67bfea
Barclay A. Dunn <barclay.dunn@mindspark.com>
2016-08-15 12:54:03 -0400

    still not working. spoofed mouseover, but dom dn change within 1000 tries

--- functionality_jswidget.js
+++ functionality_jswidget.js
@@ -1,28 +1,50 @@
 javascript:(function() {
-var notifsCount = Number(document.getElementById('notificationsCountValue').childNodes[0].nodeValue);
-var newNotifsCount = notifsCount;
-alert('newNotifsCount: ' + newNotifsCount);
-var jewelItems = document.getElementsByClassName('uiScrollableAreaContent')[0].childNodes[0].childNodes;
-for (var i = 0; i < jewelItems.length; i++) {
-  var node = jewelItems[i];
-  var dataGt = (node.attributes['data-gt'] != undefined) ? JSON.parse(node.attributes['data-gt'].nodeValue) : {};
-  if (node.classList.contains('jewelItemNew') && dataGt.hasOwnProperty('notif_type')) {
-    alert('notif type: ' + dataGt['notif_type']);
-    if (dataGt['notif_type'] == 'like' || dataGt['notif_type'] == 'feedback_reaction_generic') {
-      alert('gettinghere');
-      node.classList.remove('jewelItemNew');
-      alert('gettinghere2');
-      newNotifsCount--;
-      alert('newNotifsCount: ' + newNotifsCount);
+function simulateMouseover(node, callback) {
+  var evt = new MouseEvent('mouseover')
+  node.dispatchEvent(evt);
+  callback();
+}
+function setGrandchild() {
+    grandchild = notifsFlyout.childNodes[2].firstChild.firstChild.firstChild;
+}
+var notifsCountVal = document.getElementById('notificationsCountValue');
+var notifsCountNode = notifsCountVal.firstChild;
+var notifsCount = Number(notifsCountNode.nodeValue);
+var grandchild;
+if (notifsCount > 0) {
+  var newNotifsCount = notifsCount;
+  var jewel = document.getElementById('fbNotificationsJewel');
+  var notifsFlyout = document.getElementById('fbNotificationsFlyout');
+  simulateMouseover(jewel, setGrandchild);
+  var count = 0;
+  while (grandchild == undefined && count < 1000) {
+    setGrandchild();
+    count++;
+  }
+  var jewelItems = grandchild.firstChild.firstChild.firstChild.firstChild.childNodes;
+  for (var i = 0; i < jewelItems.length; i++) {
+    var node = jewelItems[i];
+    var dataGt = (node.attributes['data-gt'] != undefined) ? JSON.parse(node.attributes['data-gt'].nodeValue) : {};
+    if (node.classList.contains('jewelItemNew') && dataGt.hasOwnProperty('notif_type')) {
+      if (dataGt['notif_type'] == 'like' || dataGt['notif_type'] == 'feedback_reaction_generic') {
+        alert('removing jewelItemNew class');
+        node.classList.remove('jewelItemNew');
+        alert('decrementing newNotifsCount');
+        newNotifsCount--;
+      }
     }
   }
-}
-alert('newNotifsCount: ' + newNotifsCount);
-if (newNotifsCount != notifsCount) {
-  if (newNotifsCount == 0)
-    document.getElementsByClassName('jewelCount')[2].classList.remove('hasNew');
-  else if (newNotifsCount > 0)
-    document.getElementById('notificationsCountValue').childNodes[0].nodeValue = newNotifsCount;
+  if (newNotifsCount != notifsCount) {
+    if (newNotifsCount == 0) {
+      alert('removing hasNew class');
+      jewel.classList.remove('hasNew');
+      alert('adding hidden_elem class to notificationsCountValue elem');
+      notifsCountVal.classList.add('hidden_elem');
+    } else if (newNotifsCount > 0) {
+      alert('changing notifs count val');
+      notifsCountNode.nodeValue = newNotifsCount;
+    }
+  }
 }
 }
 )();
